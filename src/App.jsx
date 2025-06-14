941d7752f5057075999bf3f3a8dbd9a5e81f7d58
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-06-14 15:30:36 +0530

    fonts added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <div className=" bg-[url(../public/assets/images/png/body-bg-stars.png)] h-full w-full"></div>
+      <Navbar />
     </>
   );
 }
